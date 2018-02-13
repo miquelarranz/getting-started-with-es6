9ccdeed6a523edb0c0cad15111eae62a6d380313
miquelarranz <miquelarranz@github.com>
2018-02-13 20:37:42 +0100

    Upload the example code

---
+++ script.js
@@ -0,0 +1,33 @@
+var ageNotDefined = 'Not defined';
+
+function User (email, name, age) {
+  this.email = email;
+  this.name = name;
+  this.age = age;
+  this.books = [];
+}
+
+User.prototype.getAge = function () {
+  return (this.age) ? this.age + ' year(s)' : ageNotDefined;
+};
+
+User.prototype.addBooks = function (books) {
+  this.books = this.books.concat(books);
+};
+
+User.prototype.getBook = function (position) {
+  var title = this.books[position].title;
+  var author = this.books[position].author;
+
+  return title + ' by ' + author;
+};
+
+// These lines will help you to test the example
+var user = new User('john.doe@email.com', 'John', 22);
+user.addBooks([
+    { title: 'Getting Started With ES6', author: 'John Doe' },
+    { title: 'Javascript Superhero', author: 'John Doe' }
+]);
+
+console.log(user.getAge()); // 22 year(s)
+console.log(user.getBook(0)); // Getting Started With ES6 by Jhon Doe
